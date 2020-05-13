8d15ef46b6dc28557ea3cd760c7a3b49c5cf1706
Dutta <rdtta@mothra.clas.uiowa.edu>
2020-05-13 02:05:33 -0500

    calculate disp from timeseries- edit 2 add datenum and  SBI_Year

--- mintpy_utils/calc_disp.py
+++ mintpy_utils/calc_disp.py
@@ -22,19 +22,33 @@ def SBI_Year(imd):
     A simple script that takes in numbers on the format '19990930'
     and changes to a number format 1999.7590
 
-    imd - numpy (n,1) Vector with dates
-    out - numpy (n,1) Vector with dates
+    imd - numpy (n,1) Vector with dates in strings
+    out - numpy (n,1) Vector with dates in int
     created by Rishabh Dutta
     '''
 
-    dstr = str(imd)
+    dstr = imd
     nd = imd.shape[0]
     out = np.zeros((nd,1))
 
     for k in range(nd):
         # get first day of year, minus 1/2 a day:
-                
+        d1 = dt.strptime(dstr[k][0][0:4]+'0101', '%Y%m%d')
+        dn1 = datenum(d1) - 0.5 
+        # get last day of year, plus 0.5
+        d2 = dt.strptime(dstr[k][0][0:4]+'1231', '%Y%m%d')
+        dne = datenum(d2) + 0.5
+        # get number of days in that year:
+        ndays = dne - dn1 
 
+        # get day of year:
+        d3 = dt.strptime(dstr[k][0], '%Y%m%d')
+        doy = datenum(d3) - dn1 
+        # get fractional year:
+        fracyr = doy/ndays
+        out[k] = int(dstr[k][0])+ fracyr 
+
+    return out
 
 # work directory
 proj_dir = os.path.expanduser('/data/not_backed_up/rdtta/earthquakes/Utah2020/Sentinel/AT20/Stack/timeseries')
