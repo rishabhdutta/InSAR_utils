dd3803f900aeeafcf311e89b4da66b2380d168f8
Dutta <rishabh-dutta@uiowa.edu>
2020-07-20 11:03:17 -0500

    script calc_ALT edit 1

--- permafrost/calc_ALT.py
+++ permafrost/calc_ALT.py
@@ -31,24 +31,45 @@ geom_file = os.path.join(proj_dir, 'geo/geo_geometryRadar.h5')
 ts_file = os.path.join(proj_dir, 'geo/geo_timeseries_ramp_demErr.h5')
 maskfile = os.path.join(proj_dir, 'geo/geo_maskTempCoh.h5')
 
-with h5py.File(geom_file, "r") as f:
-    # read the geometry file
-    a_group_key = list(f.keys())[0]
-    azim_angle = list(f[a_group_key])
-    a_group_key = list(f.keys())[1]
-    height = list(f[a_group_key])
-    a_group_key = list(f.keys())[2]
-    inc_angle = list(f[a_group_key])
-    a_group_key = list(f.keys())[3]
-    latitude = list(f[a_group_key])
-    a_group_key = list(f.keys())[4]
-    longitude = list(f[a_group_key])
-
 with h5py.File(maskfile, "r") as f:
     a_group_key = list(f.keys())[0]
     maskbool = list(f[a_group_key])
 
 maskbool = np.array(maskbool)
+
+mat_c1 = sio.loadmat('subsdata.mat')
+longitude = mat_c1['lon']
+latitude = mat_c1['lat']
+subs_data = mat_c1['subs_data']
+
+seasonal_subs = subs_data[:,1]
+ifglen = np.shape(longitude)[0]
+ifgwid = np.shape(longitude)[1]
+
+def get_ALT(arg_i, ifglen, seasonal_subs):
+    '''
+    Estimate ALT from seasonal subsidence value
+    '''
+    if np.int(np.mod(arg_i, 50000)) == 0:
+        print('in loop number : ', arg_i)
+    ind_len = np.mod(arg_i, ifglen) - 1
+    if np.mod(arg_i, ifglen) == 0:
+        ind_len = ifglen - 1
+    ind_wid = np.int(np.floor(arg_i/ifglen)) - 1
+    # check if masked
+    if maskbool[ind_len, ind_wid] == False:
+        return np.nan
+    seasubs = seasonal_subs[arg_i] 
+    # mask if seasonal subsidence is negative 
+    if seasubs < 0 :
+        return np.nan 
+    # check if subsidence is too small --> porosity 0.9 
+    if seasubs <= 0.0028 :
+        return seasubs*23/(2*0.9)
+    elif seasubs >= 0.0353 :
+        return .7+ (seasubs - 0.0353)*23/(2*0.44)
+    else: 
+
 
 
 
