bb73eb03b1ca5354ed7aebb15d5f17201960d1c8
Rishabh Dutta <rishabh-dutta@uiowa.edu>
2022-03-11 17:02:03 -0600

    file for creating .slc.full 6

--- isce_mst/combine_SLCs.py
+++ isce_mst/combine_SLCs.py
@@ -38,6 +38,9 @@ for i in CoList:
         Counter += 1
 # Counter is the number of lines in the file 
 
-print(file.readlines(33))
-
+lines = file.readlines()
+for index, line in enumerate(lines):
+    print("Line {}: {}".format(index, line.strip()))
+    
+file.close()
 
