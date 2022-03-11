3330109c5a37284f22c476b9bd2480e70493d6fd
Rishabh Dutta <rishabh-dutta@uiowa.edu>
2022-03-11 17:47:46 -0600

    file for creating .slc.full 7

--- isce_mst/combine_SLCs.py
+++ isce_mst/combine_SLCs.py
@@ -50,7 +50,7 @@ for line in Lines:
     #print(sys_comm3)
     sys_comm4 = 'cd ' + SLC_dir + '/' + line.strip() + '/ && '
     #print(sys_comm4)
-    sys_comm5 = sys_comm4 + sys_comm5
+    sys_comm5 = sys_comm4 + sys_comm3
     print(sys_comm5)
 file.close()
 
