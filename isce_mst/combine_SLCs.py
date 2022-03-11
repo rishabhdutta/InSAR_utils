5dd5f75cb4c1661826db3dcbcc81f55de0b6cdad
Rishabh Dutta <rishabh-dutta@uiowa.edu>
2022-03-11 16:16:19 -0600

    file for creating .slc.full 2

--- isce_mst/combine_SLCs.py
+++ isce_mst/combine_SLCs.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue May 26 11:00:12 2020
+Created on Fri Mar 11 16:07:12 2022
 @author: duttar
 """
 
 import os 
 #import numpy as np
 import argparse
+
+parser = argparse.ArgumentParser(description='goes to SLC folder in merged and creates .slc.full files from .slc.full.vrt files')
+parser.add_argument('-p', '--path', type=float, metavar='', required=True, help='enter path to merged folder')
+
+args = parser.parse_args()
+
+merged_dir = str(args.path)
+sys_comm1 = 'cd ' + merged_dir
+os.system(sys_comm1)
+os.system('mkdir trash')
+
+
 
